c928861e4541b94ca411e5016002bf82274d6810
Alex Severino <alex@7lab.nl>
2019-06-03 16:20:32 +0200

    Finish AuthGuard

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    constructor(private router: Router,
+                private authService: AuthService) {}
 
-  constructor(private router: Router) { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
+    onLoadServers(id: number) {
+        this.router.navigate(['/servers', id, 'edit'], {
+            queryParams: { allowEdit: '11', fragment: 'loading' }
+        });
+    }
 
-  onLoadServers(id: number) {
-    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '11', fragment: 'loading'}});
-  }
+    login() {
+        this.authService.login();
+    }
+
+    logout() {
+        this.authService.logout();
+
+    }
 }
